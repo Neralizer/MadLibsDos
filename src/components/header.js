62a51e234997439c1894289577c6dff2003769b5
Neral <christian.neral@708n-j218-cn-01.canyonsdistrict.pvt>
2019-07-02 08:41:03 -0600

    created background header skews

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,8 @@ import React from "react"; //since this is a functional component and not dynami
 const Header = (title, content) => { //header component, title and content needed as arguments
     return (
         <div className="header"> 
+            <div className="header_skew"></div>
+            <div className="header_border"></div>
             <h1>{title}</h1> 
             <p>{content}</p>
         </div>
