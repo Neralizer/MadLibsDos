cdaafa4ca07326f6a9ed14ff3ffaad1bc2f1b1b6
Neral <christian.neral@708n-j218-cn-01.local>
2019-07-02 16:55:03 -0600

    implemented and styled content labels

--- src/components/content.js
+++ src/components/content.js
@@ -1,22 +1,33 @@
 import React, { Component } from "react";
 
 class Content extends Component { //since this is a dynamic component and not simply functional, like input.js, we need to give it a class name and extend component
+    constructor() {
+        super();
+        this.count = 1;
+    }
+    
+    renderLabel(data){
+        return [
+            <div className="card_content_label">{this.count++}</div>,
+            <b>{data}</b>
+        ]
+    }
+
     render() {
         const data = this.props.data; //dynamic components have props. Props are declared when the component is referred to elsewhere (e.x. <Content prop1="red"/>)
         return (
             <div className="card_content">
                 <p>
-                Ladies and gentlemen, this is <b>{data.colorOne}</b> Barber, your 
-                sportscaster, bringing you the last inning of the game between the 
-                Cleveland <b>{data.pluralNounOne}</b> and the <b>{data.adjOne}</b> Yankees. <b>{data.celebOne}</b> is pitching 
-                for the Yankees. Here's the pitch! It's a low <b>{data.adjTwo}</b> ball that just cuts the inside of 
-                the <b>{data.nounOne}</b> for a strike. That makes the count <b>{data.numOne}</b> strikes 
-                and <b>{data.numTwo}</b> balls. Now here's the next pitch. The batter swings and connects. 
-                It's a long, high <b>{data.nounTwo}</b> out to <b>{data.adjThree}</b> field. But <b>{data.celebTwo}</b> is 
-                coming up fast and has it for the second out. The next batter up is <b>{data.celebThree}</b>, the 
-                Cleveland <b>{data.adjFour}</b>-stop. Here's the pitch... and it's hit... a short ground ball 
-                to third <b>{data.nounThree}</b>. <b>{data.celebFour}</b> scoops it up and throws it to first base 
-                for an out, and the game is over. And the Yankees move into second place in the <b>{data.adjFive}</b> League!
+                Ladies and gentlemen, this is {this.renderLabel(data.colorOne)} Barber, your sportscaster, bringing you the last inning of the game between the 
+                Cleveland {this.renderLabel(data.pluralNouneOne)} and the {this.renderLabel(data.adjOne)} Yankees. {this.renderLabel(data.celebOne)} is pitching 
+                for the Yankees. Here's the pitch! It's a low {this.renderLabel(data.adjTwo)} ball that just cuts the inside of 
+                the {this.renderLabel(data.nounOne)} for a strike. That makes the count {this.renderLabel(data.numOne)} strikes 
+                and {this.renderLabel(data.numTwo)} balls. Now here's the next pitch. The batter swings and connects. 
+                It's a long, high {this.renderLabel(data.nounTwo)} out to {this.renderLabel(data.adjThree)} field. But {this.renderLabel(data.celebTwo)} is 
+                coming up fast and has it for the second out. The next batter up is {this.renderLabel(data.celebThree)}, the 
+                Cleveland {this.renderLabel(data.adjFour)}-stop. Here's the pitch... and it's hit... a short ground ball 
+                to third {this.renderLabel(data.nounThree)}. {this.renderLabel(data.celebFour)} scoops it up and throws it to first base 
+                for an out, and the game is over. And the Yankees move into second place in the {this.renderLabel(data.adjFive)} League!
                 </p>
             </div>
         )
--- src/components/input.js
+++ src/components/input.js
@@ -1,10 +1,11 @@
 import React from "react"; //since this is a functional component and not dynamic, no need to import { Component }
 
-const Input = ({title,state,name}, onChange, index) => { 
+const Input = ({title,state,name}, onChange, index) => { //the first label has the classname of either input_number gray or green, depending on state.
+                                                        //if state is blank, use gray. if not, use green.
         return (
-            <div  key={index} className="input">
-                <label className="input_number">{index+1}</label>
-                <input name={name} value={state} onChange={onChange}/>
+            <div  key={index} className="input"> 
+                <label className={`input_number ${state == "" ? "gray" : "green"}`}>{index+1}</label> 
+                <input placeholder= {title} name={name} value={state} onChange={onChange}/>
                 <label className="input_title">{title}</label>
             </div>
         )
